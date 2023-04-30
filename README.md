c0a174485b7b864200da04e36c51a4893b007bdf
Denys Shapoval <96021986+dawmsi@users.noreply.github.com>
2023-04-30 19:51:53 +0300

    update config

---
+++ README.md
@@ -0,0 +1,5 @@
+functionality:
+search
+limit
+filter: type, name
+switch image
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import path from "path"
 
 export default defineConfig({
+  base:'https://dawmsi.github.io/pokedex/',
   build: {
     outDir: path.join(__dirname, "dist"),
   },
